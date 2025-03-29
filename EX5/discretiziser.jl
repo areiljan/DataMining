45c74c8f0c2ba12b88555728de9ed90f56c46cf9
areiljan <reiljanartur@gmail.com>
2025-03-29 14:43:57 +0200

    fix: refactor discretiziser.jl to use symbols

--- EX5/discretiziser.jl
+++ EX5/discretiziser.jl
@@ -11,13 +11,13 @@ df = CSV.read(CSV_FILE, DataFrame)
 # select! modifies a df in place, NOT() removes columns
 select!(df, Not(COLUMNS_TO_REMOVE))
 
-for col in NUMERIC_TO_DISCRETIZE
+for col ∈ NUMERIC_TO_DISCRETIZE
     # get min and max values
     mn, mx = minimum(df[!, col]), maximum(df[!, col])
     edges = collect(range(mn, mx, length=N_BINS+1))
     # lambda function assignment
     discretize_val(x) = begin
-        for i in 1:(length(edges)-1)
+        for i ∈ 1:(length(edges)-1)
             if (i < length(edges)-1 && x ≥ edges[i] && x < edges[i+1]) ||
                 (i == length(edges)-1 && x ≥ edges[i] && x ≤ edges[i+1])
                 return @sprintf("%s_%.1f_%.1f", col, edges[i], edges[i+1])
@@ -25,15 +25,15 @@ for col in NUMERIC_TO_DISCRETIZE
         end
         ""
     end
-    df[!, col] = [discretize_val(x) for x in df[!, col]]
+    df[!, col] = [discretize_val(x) for x ∈ df[!, col]]
 end
 
-transactions = [ [ (col in NUMERIC_TO_DISCRETIZE ? string(row[col]) :
-    string(col, "_", row[col])) for col in names(df) if !ismissing(row[col]) ]
-for row in eachrow(df) ]
+transactions = [ [ (col ∈ NUMERIC_TO_DISCRETIZE ? string(row[col]) :
+    string(col, "_", row[col])) for col ∈ names(df) if !ismissing(row[col]) ]
+for row ∈ eachrow(df) ]
 
 open(OUTPUT_FIMI_FILE, "w") do io
-    for trans in transactions
+    for trans ∈ transactions
         println(io, join(trans, " "))
     end
 end
